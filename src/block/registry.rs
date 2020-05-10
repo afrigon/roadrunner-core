87578d9a87263dc5669256dd9e513c78d85e8346
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2020-05-09 21:50:52 -0400

    removed garbage

--- src/block/registry.rs
+++ src/block/registry.rs
@@ -11,20 +11,12 @@ impl BlockRegistry {
         Self { data }
     }
 
-    //pub fn get(&self, name: &str) -> Block {
-    // TODO: add another map to get blocks by name
-    //}
-
-    pub fn get_properties(&self, id: u8) -> Option<&BlockProperties> {
+    pub fn properties(&self, id: u8) -> Option<&BlockProperties> {
         self.data.get(&id)
     }
 
-    //pub fn register(id: u8, properties: BlockProperties) {
-    // TODO: implement this shit
-    //}
-
     pub fn is_opaque(&self, id: u8) -> bool {
-        if let Some(p) = self.get_properties(id) {
+        if let Some(p) = self.properties(id) {
             return p.opaque;
         } else {
             return false;
