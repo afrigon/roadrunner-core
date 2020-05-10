68bd86fca95016416f7a3fd958b076b320e93ec1
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2020-05-09 22:10:02 -0400

    fixed nit

--- src/world/world.rs
+++ src/world/world.rs
@@ -2,7 +2,7 @@ use crate::chunk::{Chunk, ChunkGrid, ChunkGridCoordinate, ChunkGroup};
 use crate::entity::Player;
 use crate::world::generation::WorldGenerator;
 
-const LOADED_DISTANCE: i64 = 16;
+const LOAD_DISTANCE: i64 = 16;
 
 #[derive(Default)]
 pub struct World {
@@ -23,17 +23,17 @@ impl World {
     pub fn update(&mut self, players: &Vec<Player>) {
         // (un?)load chunks as the players move
         for player in players {
-            let target_chunk = ChunkGridCoordinate::from_world_coordinate(player.get_position());
+            let target_chunk = ChunkGridCoordinate::from_world_coordinate(player.position());
             let is_near = |middle, point| -> bool {
-                (middle - LOADED_DISTANCE..middle + LOADED_DISTANCE).contains(&point)
+                (middle - LOAD_DISTANCE..middle + LOAD_DISTANCE).contains(&point)
             };
 
             self.chunks.retain(|coord, _| {
                 is_near(target_chunk.x, coord.x) && is_near(target_chunk.z, coord.z)
             });
 
-            let xrange = target_chunk.x - LOADED_DISTANCE..target_chunk.x + LOADED_DISTANCE;
-            let zrange = target_chunk.z - LOADED_DISTANCE..target_chunk.z + LOADED_DISTANCE;
+            let xrange = target_chunk.x - LOAD_DISTANCE..target_chunk.x + LOAD_DISTANCE;
+            let zrange = target_chunk.z - LOAD_DISTANCE..target_chunk.z + LOAD_DISTANCE;
 
             for x in xrange {
                 for z in zrange.clone() {
