cfc84324aa7946c15a1dc7387a97453973f6545d
filedesless <filedesless@gmail.com>
2020-05-11 03:13:42 -0400

    instrumented chunk generation

--- src/world/generation/generator.rs
+++ src/world/generation/generator.rs
@@ -2,6 +2,7 @@ use crate::block::Block;
 use crate::chunk::ChunkGridCoordinate;
 use crate::chunk::{Chunk, CHUNK_DEPTH, CHUNK_WIDTH};
 use crate::world::generation::height_map::HeightMap;
+use std::time::{Duration, Instant};
 
 pub struct WorldGenerator {
     height_map: HeightMap,
@@ -14,13 +15,17 @@ pub fn generate_chunk(coords: ChunkGridCoordinate) -> Chunk {
 
 impl WorldGenerator {
     pub fn generate_chunk(&self, coords: ChunkGridCoordinate) -> Chunk {
+        let start = Instant::now();
+        let mut spent_in_height = Duration::new(0, 0);
         let mut chunk = Chunk::new(coords);
         for x in 0..CHUNK_WIDTH {
             for z in 0..CHUNK_DEPTH {
                 let absx = x as i64 + coords.x * CHUNK_WIDTH as i64;
                 let absz = z as i64 + coords.z * CHUNK_DEPTH as i64;
 
+                let now = Instant::now();
                 let height = self.height_map.get_height(absx, absz) as usize;
+                spent_in_height += now.elapsed();
 
                 for y in 0..5 {
                     chunk.blocks[x][y][z] = Block { id: 7 };
@@ -49,6 +54,13 @@ impl WorldGenerator {
                 }
             }
         }
+
+        println!(
+            "generated {:?} in {:?} (spent {:?} in get_height)",
+            coords,
+            start.elapsed(),
+            spent_in_height
+        );
         chunk
     }
 }
