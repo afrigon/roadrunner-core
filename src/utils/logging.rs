8791bed0cc00ad9fa5785ee8df5e4716a61bce5d
filedesless <filedesless@google.com>
2020-05-30 22:54:11 -0400

    add multiple logging

--- src/utils/logging.rs
+++ src/utils/logging.rs
@@ -1,21 +1,23 @@
 use log::LevelFilter;
 use log::{Level, Metadata, Record};
-use std::fs::File;
 use std::io::Write;
 use std::sync::Mutex;
 
-struct Logger {
-    file: Mutex<File>,
+pub struct Logger<W: Write + Send + Sync> {
+    output: Mutex<W>,
 }
 
-impl log::Log for Logger {
+impl<W> log::Log for Logger<W>
+where
+    W: Write + Sync + Send,
+{
     fn enabled(&self, metadata: &Metadata) -> bool {
         metadata.level() <= Level::Info
     }
 
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
-            let mut lock = self.file.lock().unwrap();
+            let mut lock = self.output.lock().unwrap();
             writeln!(lock, "{} - {}", record.level(), record.args()).unwrap();
         }
     }
@@ -23,10 +25,40 @@ impl log::Log for Logger {
     fn flush(&self) {}
 }
 
-pub fn init() {
-    log::set_boxed_logger(Box::new(Logger {
-        file: Mutex::new(File::create("logs").unwrap()),
-    }))
-    .unwrap();
+impl<W> Logger<W>
+where
+    W: Write + Sync + Send,
+{
+    pub fn new(output: W) -> Self {
+        Self {
+            output: Mutex::new(output),
+        }
+    }
+}
+
+struct MultiLogger {
+    loggers: Vec<Box<dyn log::Log>>,
+}
+
+impl log::Log for MultiLogger {
+    fn enabled(&self, record: &log::Metadata<'_>) -> bool {
+        self.loggers.iter().any(|logger| logger.enabled(record))
+    }
+
+    fn log(&self, record: &log::Record<'_>) {
+        for logger in self.loggers.iter() {
+            logger.log(record);
+        }
+    }
+
+    fn flush(&self) {
+        for logger in self.loggers.iter() {
+            logger.flush();
+        }
+    }
+}
+
+pub fn init(loggers: Vec<Box<dyn log::Log>>) {
+    log::set_boxed_logger(Box::new(MultiLogger { loggers: loggers })).unwrap();
     log::set_max_level(LevelFilter::Info);
 }
